7150f49f5db14eff0b7e7603cda8e74918c9d384
dannyvassallo <danielvassallo87@gmail.com>
2016-05-17 11:12:40 -0400

    insta opens in pop up

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,2 +1,8 @@
 // ADD YOUR JS HERE
-console.log('loaded');
+// console.log('loaded');
+
+$(document).on('click', '.card-image a, .card-action a', function(e){
+  e.preventDefault();
+  var link = $(this).attr('href');
+  window.open(link, "Instagram", "width=500,height=800");
+});
--- source/javascripts/_init-instafeed.js
+++ source/javascripts/_init-instafeed.js
@@ -8,10 +8,13 @@ var loadButton = document.getElementById('load-more');
 var feed = new Instafeed({
   get: 'tagged',
   tagName: 'fearlessrecords',
-  clientId: ' ad5b8cadaf80454698639bb22f59a0f9',
+  sortBy: 'most-liked',
+  clientId: 'e614fac9466647bebb6a625d1d9c26e9',
   resolution: 'standard_resolution',
-  orientation: 'square',
-  template: '<div class="col s12 m4"><div class="card"><div class="card-image"><img src="{{image}}"><span class="card-title">Card Title</span></div><div class="card-content"><p>{{caption}}</p><p><i class="fa fa-heart"></i> {{likes}}</p></div><div class="card-action"><a href="{{link}}">Open Post</a></div></div></div>',
+  template: '<div class="col s12 m4"><div class="card"><div class="card-image"><a href="{{link}}"><img src="{{image}}"><span class="card-title">Card Title</span></a></div><div class="card-content"><p>{{caption}}</p></div><div class="card-action"><a href="{{link}}">Open Post</a><p class="likes"><i class="fa fa-heart"></i> {{likes}}</p></div></div></div>',
+  filter: function(image) {
+    return image.type === 'video';
+  },
   // every time we load more, run this function
   after: function() {
     // disable button if no more results to load
@@ -19,6 +22,7 @@ var feed = new Instafeed({
       loadButton.setAttribute('disabled', 'disabled');
     }
 
+    //Fix masonry layout on row load
     $("img").one("load", function() {
       fixMasonryLayout();
     }).each(function() {
