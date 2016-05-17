71da7733a968753035d3ee1e52a857416dbcdd24
dannyvassallo <danielvassallo87@gmail.com>
2016-05-17 09:25:50 -0400

    rolled back middleman version

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 ###
 # Page options, layouts, aliases and proxies
 ###
+# activate :php
+# activate :dotenv
 
 # Per-page layout changes:
 #
@@ -8,6 +10,7 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/spotify/callback/', layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
@@ -24,10 +27,10 @@ set :images_dir, 'images'
 set :fonts_dir,  'fonts-folder'
 # Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
-  set :debug_assets, true
-  activate :minify_css
-  activate :minify_html
+  # activate :livereload
+  # set :debug_assets, true
+  # activate :minify_css
+  # activate :minify_html
 end
 
 ###
@@ -44,9 +47,9 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  activate :minify_css
+  # activate :minify_css
   # Minify HTML on build
-  activate :minify_html
+  # activate :minify_html
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 end
