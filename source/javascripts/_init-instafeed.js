c3b58a370bd08994eac3c8812efb790077aee2a4
dannyvassallo <danielvassallo87@gmail.com>
2016-05-17 11:20:43 -0400

    all cards hoverable

--- source/javascripts/_init-instafeed.js
+++ source/javascripts/_init-instafeed.js
@@ -11,7 +11,7 @@ var feed = new Instafeed({
   sortBy: 'most-liked',
   clientId: 'e614fac9466647bebb6a625d1d9c26e9',
   resolution: 'standard_resolution',
-  template: '<div class="col s12 m4"><div class="card"><div class="card-image"><a href="{{link}}"><img src="{{image}}"><span class="card-title">@{{model.user.username}}</span></a></div><div class="card-content"><p>{{caption}}</p></div><div class="card-action"><a href="{{link}}">Open Post</a><p class="likes"><i class="fa fa-heart"></i> {{likes}}</p></div></div></div>',
+  template: '<div class="col s12 m4"><div class="card hoverable"><div class="card-image"><a href="{{link}}"><img src="{{image}}"><span class="card-title">@{{model.user.username}}</span></a></div><div class="card-content"><p>{{caption}}</p></div><div class="card-action"><a href="{{link}}">Open Post</a><p class="likes"><i class="fa fa-heart"></i> {{likes}}</p></div></div></div>',
   filter: function(image) {
     return image.type === 'video';
   },
